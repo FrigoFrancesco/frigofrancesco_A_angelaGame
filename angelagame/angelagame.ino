dfd156416f442ab79ef50cc8b1c6e2a9e3c63e03
Francesco Frigo <francesco.frigo@barsanti.edu.it>
2019-01-18 09:45:33 +0100

    progetto con sketch

---
+++ angelagame/angelagame.ino
@@ -0,0 +1,28 @@
+//#include <LiquidCrystal.h>
+
+int num1, num2;
+int meta=30;
+int somma=0;
+int memoria[100];
+bool vittoria1=false;
+bool vittoria2=false;
+bool giocatore2=false;
+
+void setup() 
+{
+  Serial.begin(9600);
+  
+}
+
+void loop() 
+{
+  
+}
+
+void turno()
+{
+  if(giocatore2=false)
+  {
+    
+  }
+}
